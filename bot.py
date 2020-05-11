3a5e4993afa6c3aa14a39be70734a6b50a679549
EmmS21 <emmanuel@hooros.co.za>
2020-05-11 21:52:33 +0200

    fixing message not sending

--- bot.py
+++ bot.py
@@ -111,10 +111,11 @@ def bot():
         file = urllib.request.urlopen(file_path)
         full_text = [line.decode("utf-8").replace('\n','') for line in file]
         chall = random.choice(full_text).split('|')
-        hyperlink = "{link},  Title:{text}".format(link=chall[0], text=chall[1])
-        message = 'Hi {}, here is your tutorial for the day:'.format(name_num.get(cleaned_number))
+        hyperlink = " {link},  Title:{text}".format(link=chall[0], text=chall[1])
+        message = "Hi {}, here is your tutorial for the day: ".format(name_num.get(cleaned_number))
+        concated = hyperlink+message
         try:
-            msg.body(message + ' ' + hyperlink)
+            msg.body(concated)
             user_object = Users()
             user_object.cell_number = int(cleaned_number)
             user_object.request_key = incoming_msg
@@ -182,6 +183,9 @@ def bot():
         if cleaned_number in permitted and tutorial_interactions < 1:
             file_path = "https://raw.githubusercontent.com/EmmS21/GradientBoostIntrotoDS/master/Challenges/automate-tutorials.txt"
             link_tutorials(file_path=file_path,incoming_msg=incoming_msg,cleaned_number=cleaned_number)
+            responded = True
+        else:
+            action_else()
             responded = True
     if fuzz.ratio(incoming_msg, 'learn') >= 90:
         if total_interactions < 5:
